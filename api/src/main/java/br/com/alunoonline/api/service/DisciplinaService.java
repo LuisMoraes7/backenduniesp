7a81bd41c3eb54c7f9b7793c97b80d1ff8b5f7f0
Luís Eduardo <luisedunascimento17@gmail.com>
2025-11-10 21:06:59 -0300

    disciplina crud

---
+++ api/src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -0,0 +1,47 @@
+package br.com.alunoonline.api.service;
+
+import br.com.alunoonline.api.model.Disciplina;
+import br.com.alunoonline.api.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DisciplinaService {
+
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
+    public void criarDisciplina(Disciplina disciplina){
+        disciplinaRepository.save(disciplina);
+    }
+
+    public List<Disciplina> buscarTodasDisciplinas(){
+        return disciplinaRepository.findAll();
+    }
+
+    public Optional<Disciplina> buscarDisciplinaPorId(Long id){
+        disciplinaRepository.findById(id);
+        return Optional.empty();
+    }
+
+    public void deletarDisciplinaPorId(Long id){
+        disciplinaRepository.deleteById(id);
+    }
+
+    public void atualizarDisciplina(Long id, Disciplina disciplinaAtualizada){
+        Optional<Disciplina> disciplinaDoBancoDeDados = buscarDisciplinaPorId(id);
+        if (disciplinaDoBancoDeDados.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina não encontrando no banco de dados");
+        }
+        Disciplina disciplinaParaEditar = disciplinaDoBancoDeDados.get();
+        disciplinaParaEditar.setNomeDisciplina(disciplinaAtualizada.getNomeDisciplina());
+        disciplinaParaEditar.setProfessor(disciplinaAtualizada.getProfessor());
+        
+        disciplinaRepository.save(disciplinaParaEditar);
+    }
+}
